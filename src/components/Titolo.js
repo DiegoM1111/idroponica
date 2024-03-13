aab23d269c349d7478a3c4676bf5a9460dd38808
DiegoM1111 <diegomilli03@gmail.com>
2024-03-13 11:03:19 +0100

    ...

--- src/components/Titolo.js
+++ src/components/Titolo.js
@@ -6,13 +6,11 @@ function Titolo() {
     return (
         <div id="titolo">
             <h1>Idroponica</h1>
-            
-            <div className='container'>
-                <div id="imgIdroponica">
-                    <img id="img" src={imgIdroponica} alt="idroponica"/>
-                </div>
-                <a href="https://it.wikipedia.org/wiki/Idroponica"> Pagina Wikipedia sull'idroponica</a>
+
+            <div id="imgIdroponica">
+                <img id="img" src={imgIdroponica} alt="idroponica"/>
             </div>
+            <a href="https://it.wikipedia.org/wiki/Idroponica"> Pagina Wikipedia sull'idroponica</a>
         </div>
     );
 }
