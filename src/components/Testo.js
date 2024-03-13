e9052e020e4b895107a453784483b0c2722d008f
GianMarco092003 <gianmarco.cavallaro09@gmail.com>
2024-03-13 10:57:55 +0100

    .....

---
+++ src/components/Testo.js
@@ -0,0 +1,15 @@
+import "./testo.css";
+export default function Testo(){
+    return(
+        <>
+            <h1>Cosa è l'idroponica</h1>
+            <div>
+                <p>Per coltivazione idroponica (dal greco antico hydor ,acqua + ponos,lavoro)o idrocoltura s'intende una delle
+                    tecniche di coltivazione fuori suolo:la terra è costituita da un substrato inerte(argilla espansa, perlite,
+                    vermiculite, ecc..).La cultura idroponica consente produzioni controllate sia dal punto di vista qualitativo 
+                    sia da quello igenico-sanitario durante tutto l'anno.
+                </p>
+            </div>
+        </>
+    );
+}
